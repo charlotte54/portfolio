588dc35f6edcea68c32b9f8db0ef8242a87507bb
charlotte8592 <charlotte8592@gmail.com>
2022-10-12 19:07:51 +0100

    Commit

--- src/data/BlogData.js
+++ src/data/BlogData.js
@@ -5,7 +5,7 @@ export const Blogs = [
     {
         id:1,
         name:"MERN E-Commerce Website",
-        tags:["React.js","express", "MongoDB","Node.js"],
+        tags:["React","express", "MongoDB","Node"],
         imgSrc:"https://i.imgur.com/y3WMiE1.jpeg",
         link:"https://pricingcalculatorapp.herokuapp.com/",
         github:"https://github.com/charlotte54/pricingcalculator"
@@ -13,23 +13,23 @@ export const Blogs = [
     {
         id:2,
         name:"Solidity Web3 App",
-        tags:["solidity", "web3", "blockchain", "React.js"],
+        tags:["solidity", "web3", "blockchain", "React"],
         imgSrc:"https://i.imgur.com/QxLZ2oo.jpeg",
         link:"https://waveportal.charlotte31.repl.co/",
         github:"https://github.com/charlotte54/waveportal"
     },
     {
         id:3,
-        name:"NFT App",
-        tags:["NFT", "web3", "blockchain","OpenSea", "React.js"],
+        name:"Solana Web3 App",
+        tags:["Solana", "web3", "blockchain","Rust", "React"],
         imgSrc:"",
-        link:"",
+        link:"https://gif-portal.charlotte31.repl.co/",
         github:""
     },
     {
         id:4,
         name:"React Portfolio Website",
-        tags:["React.js","styled-components", "Netlify"],
+        tags:["React","styled-components", "Netlify"],
         imgSrc:"https://i.imgur.com/YMtwsI5.jpeg",
         link:"/",
         github:"https://github.com/charlotte54/portfolio"
